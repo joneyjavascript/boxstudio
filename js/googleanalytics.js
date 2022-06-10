92c207bcac43c874b876ee489430e58544d942ef
LucyJS <lucyluney@gmail.com>
2022-06-09 23:04:12 -0300

    refactoring: Script google analytics extraido do html para arquivo .js

---
+++ js/googleanalytics.js
@@ -0,0 +1,6 @@
+
+/*! Global site tag (gtag.js) - Google Analytics */
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-TS8K625S5M');
